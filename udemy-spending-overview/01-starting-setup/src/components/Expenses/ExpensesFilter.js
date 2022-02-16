25e3dda07c22d0c250dad0982be9abb234f0ca7e
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-02-16 09:57:53 +0100

    working with state, child-parent rship

--- udemy-spending-overview/01-starting-setup/src/components/Expenses/ExpensesFilter.js
+++ udemy-spending-overview/01-starting-setup/src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const [enteredYear, setEnteredYear] = useState("");
-
   const dropdownChangeHandler = (event) => {
-    setEnteredYear(event.target.value);
+    props.onChangeFilter(event.target.value);
   };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
