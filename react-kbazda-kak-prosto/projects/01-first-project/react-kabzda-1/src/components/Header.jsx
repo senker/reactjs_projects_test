185fceb039c612f01f5c5c25785d34afbbb31670
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-02-22 14:56:14 +0100

    component split

---
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://api.freelogodesign.org/assets/thumb/logo/15101367_400.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+      </div>
+      <div> ava + description</div>
+      <div>
+        {" "}
+        my posts
+        <div> new post</div>
+        <div>
+          {" "}
+          <div> post 1</div> <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
