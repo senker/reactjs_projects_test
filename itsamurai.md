94acac1c0af2b91ff97867e94ed241d6acf35f9d
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-02-22 18:34:12 +0100

    css file structurization

---
+++ itsamurai.md
@@ -0,0 +1,18 @@
+1. Why NodeJS on frontend? 
+Code written in javascript is interpretted by the browser(chrome)
+Code written in javascript can be also interpretted by node JS.
+The thing the have in common is the V8 engine.
+
+NodeJs is a platform that understands javascript and uses it for its goals.
+NodeJs is used for work with files, databases.
+
+Node.js server javascript
+
+npm install --- -save (saves it in the package.json)
+
+gitk --all&
+
+git checkout SHAID
+git status
+
+git checkout sec/App.js (will revert the file to the original state of the version of commit)
--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
       </div>
       <div> ava + description</div>
       <div>
-        {" "}
         my posts
         <div> new post</div>
-        <div>
-          {" "}
-          <div> post 1</div> <div>post 2</div>
+        <div className={s.posts}>
+          <div className={s.item}>post 1</div>
+          <div className={s.item}>post 2</div>
         </div>
       </div>
     </div>
