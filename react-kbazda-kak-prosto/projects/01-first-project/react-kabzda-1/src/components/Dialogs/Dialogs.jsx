b75c66767588d65a48c4c49d172aa62d79ce74d8
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-03-01 13:24:08 +0100

    business logic layer analysis and user interface refactoring

--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -19,20 +19,68 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    {
+      id: 1,
+      name: "Vladimir",
+    },
+    {
+      id: 2,
+      name: "Andrei",
+    },
+    {
+      id: 3,
+      name: "Svetlana",
+    },
+    {
+      id: 4,
+      name: "Sanya",
+    },
+    {
+      id: 5,
+      name: "Victor",
+    },
+    {
+      id: 6,
+      name: "Valera",
+    },
+  ];
+  let messagesData = [
+    {
+      id: 1,
+      message: "Hello",
+    },
+    {
+      id: 2,
+      message: "How are you?",
+    },
+    {
+      id: 3,
+      message: "Good, thank you, and you?",
+    },
+    {
+      id: 4,
+      message: "Good, thank you",
+    },
+    {
+      id: 5,
+      message: "Bye",
+    },
+    {
+      id: 6,
+      message: "Bye bye",
+    },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogItem name="Vladimir" id="1" />
-        <DialogItem name="Andrei" id="2" />
-        <DialogItem name="Svetlana" id="3" />
-        <DialogItem name="Sanya" id="4" />
-        <DialogItem name="Victor" id="5" />
-        <DialogItem name="Valera" id="6" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
       </div>
       <div className={s.messages}>
-        <Message message="Hi" />
-        <Message message="How are you?" />
-        <Message message="Yoyoyo" />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
       </div>
     </div>
   );
--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,19 @@ import Post from "./Post/Post";
 import s from "./MyPosts.module.css";
 
 const MyPosts = (props) => {
+  let postData = [
+    {
+      id: 1,
+      message: "Hi, how are you?",
+      likesCount: 12,
+    },
+    {
+      id: 2,
+      message: "It's my first post",
+      likesCount: 25,
+    },
+  ];
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -14,8 +27,14 @@ const MyPosts = (props) => {
         <button>Remove</button>
       </div>
       <div className={s.posts}>
-        <Post message="Hi, how are you?" likesCount="15" />
-        <Post message="It's my first post" likesCount="25" />
+        <Post
+          message={postData[0].message}
+          likesCount={postData[0].likesCount}
+        />
+        <Post
+          message={postData[1].message}
+          likesCount={postData[1].likesCount}
+        />
       </div>
     </div>
   );
