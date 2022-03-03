1fb95a403a07d15bd2fe1777fa816ecdd87d350f
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-03-03 09:07:00 +0100

    mapping of elements and updating notes

--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -19,68 +19,41 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  let dialogsData = [
-    {
-      id: 1,
-      name: "Vladimir",
-    },
-    {
-      id: 2,
-      name: "Andrei",
-    },
-    {
-      id: 3,
-      name: "Svetlana",
-    },
-    {
-      id: 4,
-      name: "Sanya",
-    },
-    {
-      id: 5,
-      name: "Victor",
-    },
-    {
-      id: 6,
-      name: "Valera",
-    },
+  let dialogs = [
+    { id: 1, name: "Vladimir" },
+    { id: 2, name: "Andrei" },
+    { id: 3, name: "Svetlana" },
+    { id: 4, name: "Sanya" },
+    { id: 5, name: "Victor" },
+    { id: 6, name: "Valera" },
   ];
-  let messagesData = [
-    {
-      id: 1,
-      message: "Hello",
-    },
-    {
-      id: 2,
-      message: "How are you?",
-    },
-    {
-      id: 3,
-      message: "Good, thank you, and you?",
-    },
-    {
-      id: 4,
-      message: "Good, thank you",
-    },
-    {
-      id: 5,
-      message: "Bye",
-    },
-    {
-      id: 6,
-      message: "Bye bye",
-    },
+
+  let messages = [
+    { id: 1, message: "Hello" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Good, thank you, and you?" },
+    { id: 4, message: "Good, thank you" },
+    { id: 5, message: "Bye" },
+    { id: 6, message: "Bye bye" },
   ];
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
+        {dialogsElements}
+        {/* Old way of doing this, hard coded 
         <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} /> */}
       </div>
       <div className={s.messages}>
+        {messagesElements}
+        {/* Old way of doing t his, hard coded way
         <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
+        <Message message={messagesData[1].message} /> */}
       </div>
     </div>
   );
--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import Post from "./Post/Post";
 import s from "./MyPosts.module.css";
 
 const MyPosts = (props) => {
-  let postData = [
+  let posts = [
     {
       id: 1,
       message: "Hi, how are you?",
@@ -15,7 +15,9 @@ const MyPosts = (props) => {
       likesCount: 25,
     },
   ];
-
+  let postsElements = posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -26,16 +28,7 @@ const MyPosts = (props) => {
         <button>Add post</button>
         <button>Remove</button>
       </div>
-      <div className={s.posts}>
-        <Post
-          message={postData[0].message}
-          likesCount={postData[0].likesCount}
-        />
-        <Post
-          message={postData[1].message}
-          likesCount={postData[1].likesCount}
-        />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
