d35f885f1fe8b879ce7ff0b937324bfdae461fbb
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-03-07 14:45:50 +0100

    adding render class and flux architecture analysis

--- react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/Profile.jsx
+++ react-kbazda-kak-prosto/projects/01-first-project/react-kabzda-1/src/components/Profile/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import s from "./Profile.module.css";
 
 const Profile = (props) => {
   return (
